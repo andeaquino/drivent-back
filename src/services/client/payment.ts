b9ab8fd8bd62777e63e6ed1d40f9f755931f8a6d
Pedro Lucas <pedrolucaspina22@gmail.com>
2022-01-20 08:39:51 -0300

    feat: add payment controller

--- src/services/client/payment.ts
+++ src/services/client/payment.ts
@@ -4,6 +4,7 @@ import PresenceType from "@/entities/PresenceType";
 export async function getAllPlans() {
   const hotelPlans =  await HotelPlan.getAll();
   const presenceTypes = await PresenceType.getAll();
+  
   return {
     hotelPlans: [...hotelPlans],
     presenceTypes: [...presenceTypes]
