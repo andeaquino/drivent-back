0b3967b5652cb2c3ee0e8065cc0c0437cc2b332f
Kadugs <cadu20.gomes@gmail.com>
2022-01-21 16:40:05 -0300

    feat: create booking

--- src/controllers/client/booking.ts
+++ src/controllers/client/booking.ts
@@ -5,10 +5,27 @@ import * as service from "@/services/client/booking";
 export async function get(req: Request, res: Response, next: NextFunction) {
   try {
     const userId = req.user.id;
-
     const booking = await service.getBooking(userId);
 
     res.send(booking);
+  } catch (e) {
+    if (e.name === "PaymentRequired") {
+      return res.status(402).send(e.message);
+    }
+    if (e.name === "PreconditionFailed") {
+      return res.status(412).send(e.message);
+    } else res.send("Não foi possível conectar ao servidor!");
+    next(e);
+  }
+}
+
+export async function post(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { userId, roomId } = req.body;
+
+    await service.postBooking({ userId, roomId });
+
+    res.sendStatus(200);
   } catch (e) {
     if (e.name === "PaymentRequired") {
       return res.status(402).send(e.message);
--- src/routers/client/booking.ts
+++ src/routers/client/booking.ts
@@ -5,5 +5,6 @@ import * as controller from "@/controllers/client/booking";
 const router = Router();
 
 router.get("/", controller.get);
+router.post("/", controller.post);
 
 export default router;
